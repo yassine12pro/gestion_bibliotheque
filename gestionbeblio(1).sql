1b5af53d7f5367e4f99eed30deb47c06f6309430
yassine12pro <yassinebelkhiria23@gmail.com>
2024-12-02 11:25:59 +0100

    update DB

--- gestionbeblio(1).sql
+++ gestionbeblio(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : dim. 01 déc. 2024 à 13:30
+-- Généré le : lun. 02 déc. 2024 à 10:24
 -- Version du serveur : 8.0.37-0ubuntu0.23.10.2
 -- Version de PHP : 8.2.10-2ubuntu2.2
 
@@ -48,6 +48,33 @@ INSERT INTO `Auteur` (`id`, `nom`, `biographie`, `date_de_naissance`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `commentaires`
+--
+
+CREATE TABLE `commentaires` (
+  `id` int NOT NULL,
+  `livre_id` int NOT NULL,
+  `commentaire` text NOT NULL,
+  `date_creation` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `commentaires`
+--
+
+INSERT INTO `commentaires` (`id`, `livre_id`, `commentaire`, `date_creation`) VALUES
+(1, 20, 'testetesttest', '2024-12-02 10:26:22'),
+(2, 20, 'test', '2024-12-02 10:26:47'),
+(3, 14, 'blabla', '2024-12-02 10:27:27'),
+(4, 16, 'testtesttest', '2024-12-02 10:31:39'),
+(5, 14, 'blellllllllll', '2024-12-02 10:35:04'),
+(6, 17, 'c est un tres bon livre', '2024-12-02 10:42:23'),
+(7, 18, 'non ce ne pas bon', '2024-12-02 10:43:29'),
+(8, 20, 'xyzxyz', '2024-12-02 10:45:06');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `Emprunt`
 --
 
@@ -63,7 +90,8 @@ CREATE TABLE `Emprunt` (
 --
 
 INSERT INTO `Emprunt` (`id`, `livre_id`, `utilisateur_id`, `date_emprunt`) VALUES
-(16, 16, 3, '2024-12-01');
+(16, 16, 3, '2024-12-01'),
+(17, 19, 3, '2024-12-02');
 
 -- --------------------------------------------------------
 
@@ -108,7 +136,8 @@ CREATE TABLE `historique_emprunts` (
 INSERT INTO `historique_emprunts` (`id`, `utilisateur_id`, `livre_id`, `date_emprunt`, `date_retour`) VALUES
 (1, 3, 16, '2024-12-01', '2024-12-01'),
 (2, 3, 17, '2024-12-01', '2024-12-01'),
-(3, 3, 16, '2024-12-01', NULL);
+(3, 3, 16, '2024-12-01', NULL),
+(4, 3, 19, '2024-12-02', NULL);
 
 -- --------------------------------------------------------
 
@@ -132,9 +161,11 @@ CREATE TABLE `Livre` (
 
 INSERT INTO `Livre` (`id`, `titre`, `auteur_id`, `genre_id`, `ISBN`, `disponible`, `image`) VALUES
 (14, 'blaaaaaa', 1, 1, '1121', 1, 'uploads/emblem-best-quality-number-1-png_62239.jpg'),
-(16, 'liv1', 1, 11, '1211', 0, 'uploads/img1.jpeg'),
+(16, 'liv111', 1, 11, '1211', 0, 'uploads/img1.jpeg'),
 (17, 'liv2', 2, 8, '233', 1, 'uploads/img2.jpeg'),
-(18, 'test2', 2, 12, '121', 1, 'uploads/img3.jpeg');
+(18, 'test2', 2, 12, '121', 1, 'uploads/img3.jpeg'),
+(19, 'livX', 2, 11, '1122', 0, 'uploads/img1.jpeg'),
+(20, 'livY', 10, 12, '10100', 1, 'uploads/img3.jpeg');
 
 -- --------------------------------------------------------
 
@@ -170,6 +201,13 @@ INSERT INTO `Utilisateur` (`id`, `nom`, `email`, `phone`, `password`, `role`) VA
 --
 ALTER TABLE `Auteur`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `livre_id` (`livre_id`);
 
 --
 -- Index pour la table `Emprunt`
@@ -220,10 +258,16 @@ ALTER TABLE `Auteur`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
+-- AUTO_INCREMENT pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT pour la table `Emprunt`
 --
 ALTER TABLE `Emprunt`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `Genre`
@@ -235,13 +279,13 @@ ALTER TABLE `Genre`
 -- AUTO_INCREMENT pour la table `historique_emprunts`
 --
 ALTER TABLE `historique_emprunts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `Livre`
 --
 ALTER TABLE `Livre`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pour la table `Utilisateur`
@@ -252,6 +296,12 @@ ALTER TABLE `Utilisateur`
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`livre_id`) REFERENCES `Livre` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `Emprunt`
